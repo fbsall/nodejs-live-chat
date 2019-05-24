7ff6f9ba3ef65e90f1a4e8a45fed05162bd3f95b
fbsall <fbsall@outlook.com>
2019-05-24 13:16:35 +0000

    Real-time Message Management

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'),
+
+//Load index page
+app.get('/', function(req, res){
+  res.sendfile(__dirname + "/index.html");
+});
+
+io.socket.on('login', function(socket, username){
+  //New client connexion message
+  socket.on('new_user', function(username){
+    username = ent.encode(username);
+    socket.username = username;
+    socket.braocast.emit('new_user', username);
+  });
+
+  //User send message to others clients
+  socket.on('message', function(){
+    message = ent.encode(message);
+    socket.braocast.emit('message', {username: socket.username, message: message});
+  });
+
+});
+
+server.listen(8080); 
